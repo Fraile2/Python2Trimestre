edee80db1a52fb9fa048aac24c9e723fe4808427
Fraile2 <alejandrofraile420@gmail.com>
2024-01-11 10:53:02 +0100

    Inicio

---
+++ 2trimestre/diccionario.py
@@ -0,0 +1,11 @@
+alumno1={
+    "Nombre": "Alejandro",
+    "Edad":20,
+    "Soltero": False
+}
+
+alumno1["Apellidos"]="Fraile"
+print(alumno1)
+
+alumno1["Apellidos"]="Fraile del Olmo"
+print(alumno1)
